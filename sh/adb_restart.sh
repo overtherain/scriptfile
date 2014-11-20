44d3c9cc5953fcf6a7faca3ba1001f279f1179a4
M.R.Z <zgd1348833@gmail.com>
2014-11-20 21:59:09 +0800

    If have add setting rules in /etc/udev/rules, just count adb root damon. If count is 0, just start user permission service. If ct is not 0, just start a root permission service.

--- sh/adb_restart.sh
+++ sh/adb_restart.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-echo sudo `which adb` kill-server
-sudo `which adb` kill-server
-echo sudo `which adb` start-server
-sudo `which adb` start-server
-echo sudo `which adb` devices
-sudo `which adb` devices
+ct=`ps aux | grep adb | grep root | wc -l`
+lsusb
+echo adbd count is : $ct
+case $ct in
+	0)
+		echo No need root permission
+		echo `which adb` kill-server
+		`which adb` kill-server
+		echo `which adb` start-server
+		`which adb` start-server
+		echo `which adb` devices
+		`which adb` devices
+		;;
+	*)
+		echo Need root permission
+		echo sudo `which adb` kill-server
+		sudo `which adb` kill-server
+		echo sudo `which adb` start-server
+		sudo `which adb` start-server
+		echo sudo `which adb` devices
+		sudo `which adb` devices
+		;;
+esac
