3662968aec3ab1d8e0876e29e7e0d352ee9c774c
M.R.Z <zgd1348833@gmail.com>
2014-12-05 22:44:56 +0800

    1.update mount, show command detail 2.Add user3 3.can use param to eclipse

--- sh/eclipse.php.sh
+++ sh/eclipse.php.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+param=$1
 cd ${MSOFT}/eclipse.php
 export JAVA_HOME=/usr/lib/jvm/openjdk-7-amd64
 export PATH=${JAVA_HOME}/bin:$PATH
-./eclipse &
+./eclipse $param &
--- sh/sshfs_mount.sh
+++ sh/sshfs_mount.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 #read -p "Connect shareusr to local:" ch
 ch=$1
+echo USER:$ch
 case $ch in
 	y|Y|yes|Yes|YES)
 		echo "Mount share to local:"
+		echo sshfs ${BUILD_SERVER_USER0}@${BUILD_SERVER}:/home/${BUILD_SERVER_USER0} ${MNT_ROOT}/${BUILD_SERVER_USER0}
 		sshfs ${BUILD_SERVER_USER0}@${BUILD_SERVER}:/home/${BUILD_SERVER_USER0} ${MNT_ROOT}/${BUILD_SERVER_USER0}
 		;;
 	${BUILD_SERVER_USER1})
 		echo "Mount yjx to local:"
+		echo sshfs ${BUILD_SERVER_USER1}@${BUILD_SERVER}:/home/${BUILD_SERVER_USER1} ${MNT_ROOT}/${BUILD_SERVER_USER1}
 		sshfs ${BUILD_SERVER_USER1}@${BUILD_SERVER}:/home/${BUILD_SERVER_USER1} ${MNT_ROOT}/${BUILD_SERVER_USER1}
 		;;
 	${BUILD_SERVER_USER2})
 		echo "Mount xyq to local:"
+		echo sshfs ${BUILD_SERVER_USER2}@${BUILD_SERVER}:/home/${BUILD_SERVER_USER2} ${MNT_ROOT}/${BUILD_SERVER_USER2}
 		sshfs ${BUILD_SERVER_USER2}@${BUILD_SERVER}:/home/${BUILD_SERVER_USER2} ${MNT_ROOT}/${BUILD_SERVER_USER2}
+		;;
+	${BUILD_SERVER_USER3})
+		echo "Mount yjx to local:"
+		echo sshfs ${BUILD_SERVER_USER3}@${BUILD_SERVER}:/home/${BUILD_SERVER_USER3} ${MNT_ROOT}/${BUILD_SERVER_USER3}
+		sshfs ${BUILD_SERVER_USER3}@${BUILD_SERVER}:/home/${BUILD_SERVER_USER3} ${MNT_ROOT}/${BUILD_SERVER_USER3}
 		;;
 	*)
 		echo "Mount self to local:"
+		echo sshfs ${BUILD_SERVER_USER}@${BUILD_SERVER}:/home/${BUILD_SERVER_USER} ${MNT_ROOT}/server
 		sshfs ${BUILD_SERVER_USER}@${BUILD_SERVER}:/home/${BUILD_SERVER_USER} ${MNT_ROOT}/server
 		;;
 esac
